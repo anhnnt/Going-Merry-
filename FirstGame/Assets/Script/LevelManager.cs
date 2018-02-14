ab5be72190b542ec5514151cecc14869370a3e29
Shujie Deng <shujied@metropolia.fi>
2018-02-14 15:57:21 +0800

    FirstGame

---
+++ FirstGame/Assets/Script/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public float waitToRespawn;
+	public PlayerController thePlayer;
+
+	public GameObject deathSplosion;
+	// Use this for initialization
+	void Start () {
+		thePlayer = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Respawn()
+	{
+		StartCoroutine ("RespawnCo");
+	}
+
+	public IEnumerator RespawnCo()
+	{
+		thePlayer.gameObject.SetActive (false);
+		Instantiate (deathSplosion, thePlayer.transform.position, thePlayer.transform.rotation); 
+		yield return new WaitForSeconds (waitToRespawn);
+
+		thePlayer.transform.position = thePlayer.respawnPosition;
+		thePlayer.gameObject.SetActive (true);
+	}
+}
---
+++ FirstGame/Assets/Script/playerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour {
+
+	private bool attacking = false; 
+
+	private float attackTimer = 0;
+	private float attackCd = 0.3f;
+
+	public Collider2D attackTrigger;
+
+	private Animator anim;
+
+	void Awake()
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		attackTrigger.enabled = false;
+	}
+
+	void Update()
+	{	
+		//collider box ON
+		if (Input.GetButtonDown("Fire1") && !attacking) 
+		{
+			attacking = true;
+			attackTimer = attackCd;
+			attackTrigger.enabled = true;
+		}
+
+
+		if (attacking)
+		{
+			if (attackTimer > 0) 
+			{
+				attackTimer -= Time.deltaTime;
+			} else 
+			{
+				attacking = false;
+				attackTrigger.enabled = false;
+			}
+		}
+
+		//assign attack animation
+		anim.SetBool ("Attack", attacking);	
+	}
+
+}
