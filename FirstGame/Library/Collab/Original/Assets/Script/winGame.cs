006a2b00ddce9c7afd9d2efef6f17d9da4f8771f
Anh Nguyen <33486007+anhnnt@users.noreply.github.com>
2018-03-13 11:45:46 +0200

    Add files via upload

---
+++ FirstGame/Library/Collab/Original/Assets/Script/winGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            // Destroy(GameObject);
+            SceneManager.LoadScene("MainMenu");
+            // Time.timeScale = 5.0f;
+        }
+    }
+
+    /*  // Update is called once per frame
+      public void win(bool Quit)
+      {
+          if (Quit)
+          {
+              Time.timeScale = 1;
+              SceneManager.LoadScene("MainMenu");
+          }
+      } */
+    }
+
